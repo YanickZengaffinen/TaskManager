5895a7bdf72b060f68e5c70ea5e15ffce9dcd048
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2020-04-13 15:19:20 +0200

    Cleaned Up Imports

--- taskmanager-client/src/Navigation.js
+++ taskmanager-client/src/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export class Navigation extends Component {
   displayName = Navigation.name
